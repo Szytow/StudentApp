5eb259b60f19e9c39f232f66fe2d5955f6189269
maciejszytow <maciejszytow@wifi-newelska-nat-28.wsisiz.edu.pl>
2018-11-05 10:35:38 +0100

    First commit

---
+++ src/studentapp/StudentApp.java
@@ -0,0 +1,12 @@
+
+package studentapp;
+import java.awt.*;
+import javax.swing.*;
+
+public class StudentApp extends JFrame{
+    
+    public static void main(String[] args) {
+        new StudentFrame().setVisible(true);
+    }
+    
+}
